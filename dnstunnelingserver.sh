5b3eeca02f04ade77289d441c023693accf47fcb
hackingyseguridad <antonio_taboada@hotmail.es>
2019-04-09 11:40:07 +0200

    Create dnstunnelingserver.sh

---
+++ dnstunnelingserver.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo 
+echo "Muestra los resolver que tenemos configuracdos"
+cat /etc/resolv.conf
+echo 
+echo "Ponemos puerto dns 53 udp escucha"
+dns2tcpd -f /etc/dns2tcpd.conf &
+echo
+echo "Comprueba que el puerto domain esta en escucha"
+netstat -a |grep domain
